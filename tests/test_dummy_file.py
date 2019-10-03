4f8e2ed04111edc5ae657c16a419b6b9017122ca
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-10-03 22:42:39 +0100

    Check how to remove virtual env from tests.

--- tests/test_dummy_file.py
+++ tests/test_dummy_file.py
@@ -5,5 +5,5 @@ def test_dummy_function_less_or_equal():
     assert dummy_function_to_test(10) == 'less than or equal to ten'
 
 
-def test_dummy_function_greater():
-    assert dummy_function_to_test(11) == 'greater than ten'
+# def test_dummy_function_greater():
+#     assert dummy_function_to_test(11) == 'greater than ten'
