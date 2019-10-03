a8688fead6319c30f4c4a736a267d629455f5e1a
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-10-03 22:52:33 +0100

    Remove dummy files.

--- dummy_package/dummy_file.py
+++
@@ -1,4 +0,0 @@
-def dummy_function_to_test(value: int):
-    if value > 10:
-        return 'greater than ten'
-    return 'less than or equal to ten'
--- tests/test_dummy_file.py
+++
@@ -1,9 +0,0 @@
-from dummy_package.dummy_file import dummy_function_to_test
-
-
-def test_dummy_function_less_or_equal():
-    assert dummy_function_to_test(10) == 'less than or equal to ten'
-
-
-# def test_dummy_function_greater():
-#     assert dummy_function_to_test(11) == 'greater than ten'
