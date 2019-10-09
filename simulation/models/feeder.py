7c4a2ad74b1c147caf4a2264881e0fa2d25c13bd
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-10-09 22:36:26 +0100

    Fix typing.

--- simulation/models/feeder.py
+++ simulation/models/feeder.py
@@ -1,5 +1,5 @@
 import random
-from typing import Union, Iterable, Sequence
+from typing import Union, Sequence, Iterator
 
 from simulation.exceptions.exceptions import FeederConfigError
 from simulation.exceptions.messages import INVALID_FEED_INPUT
@@ -14,18 +14,18 @@ class Feeder(BaseModel):
     Use to provide feed for the conveyor belt. If no feed function specified will select random item from components
     list and return every time feed() is called on an instance.
     """
-    def __init__(self, feed_input: Union[Iterable['Component'], Sequence['Component']] = None, *args, **kwargs):
+    def __init__(self, feed_input: Union[Iterator['Component'], Sequence['Component']] = None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.__feed_input = self.__get_feed_input(feed_input) if feed_input else self.__default_feed_input()
 
-    def __default_feed_input(self):
+    def __default_feed_input(self) -> Iterator['Component']:
         while True:
             yield random.choice(self.components.all())
 
     @staticmethod
     def __get_feed_input(
-            feed_input: Union[Iterable['Component'], Sequence['Component']]
-    ) -> Iterable['Component']:
+            feed_input: Union[Iterator['Component'], Sequence['Component']]
+    ) -> Iterator['Component']:
         if hasattr(feed_input, '__next__'):
             return feed_input
         try:
