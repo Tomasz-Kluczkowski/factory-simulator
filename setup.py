887ad8fc271a2183e4c1cd44de32f9044911c79f
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-10-19 13:59:43 +0100

    Remove folder.

---
+++ setup.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Empty setup.py to enable tox, but without installing
+anything - this is a test package."""
+import setuptools
+# stub out the setup - otherwise tox won't
+setuptools.setup()
