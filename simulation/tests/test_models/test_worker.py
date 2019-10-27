e26b4864f09d333b59d914da9738005759cc4e38
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-10-27 21:27:50 +0000

    Complete reworking Worker tests.

--- simulation/tests/test_models/test_worker.py
+++ simulation/tests/test_models/test_worker.py
@@ -77,38 +77,31 @@ class TestWorker:
 
         assert worker.items == []
 
-    # def test_completes_drop_product_operation_in_correct_time(
-    #         self, worker_factory, basic_conveyor_belt, worker_operation_times
-    # ):
-    #     worker_operation_times.PICKING_UP = 1
-    #     worker = worker_factory(
-    #         conveyor_belt=basic_conveyor_belt,
-    #         operation_times=worker_operation_times
-    #     )
-    #
-    #     feed_items = ['A', 'B', 'E', 'E', 'E', 'E']
-    #     for feed_item in feed_items:
-    #         basic_conveyor_belt.enqueue(feed_item)
-    #         basic_conveyor_belt.dequeue()
-    #         worker.work()
-    #     assert worker.components == ['P']
-    #
-    #     for i in range(worker_operation_times.DROPPING):
-    #         worker.work()
-    #
-    #     assert worker.components == []
-    #     assert basic_conveyor_belt.is_slot_free(slot_number=0)
-    #
-    # def test_cannot_drop_product_if_slot_is_used_by_another_worker(self, worker_factory, basic_conveyor_belt):
-    #     worker: Worker = worker_factory(conveyor_belt=basic_conveyor_belt)
-    #
-    #     feed_items = ['A', 'B', 'E', 'E', 'E', 'E']
-    #     for feed_item in feed_items:
-    #         basic_conveyor_belt.enqueue(feed_item)
-    #         basic_conveyor_belt.dequeue()
-    #         worker.work()
-    #
-    #     basic_conveyor_belt.put_item_in_slot(slot_number=0, item='A')
-    #     worker.work()
-    #
-    #     assert worker.components == ['P']
+    def test_it_completes_drop_product_operation_in_correct_time(self, worker, conveyor_belt):
+        feed_item_names = ['A', 'B', 'E', 'E', 'E', 'E']
+        for feed_item_name in feed_item_names:
+            component = ComponentFactory(name=feed_item_name)
+            conveyor_belt.enqueue(component)
+            conveyor_belt.dequeue()
+            worker.work()
+        assert worker.item_names == ['P']
+
+        for i in range(worker.operation_times.drop_time):
+            worker.work()
+
+        assert worker.items == []
+        assert conveyor_belt.is_slot_free(slot_number=0)
+
+    def test_it_cannot_drop_product_if_slot_is_used_by_another_worker(self, worker, conveyor_belt):
+        feed_item_names = ['A', 'B', 'E', 'E', 'E', 'E']
+        for feed_item_name in feed_item_names:
+            component = ComponentFactory(name=feed_item_name)
+            conveyor_belt.enqueue(component)
+            conveyor_belt.dequeue()
+            worker.work()
+
+        component = ComponentFactory(name='A')
+        conveyor_belt.put_item_in_slot(slot_number=0, item=component)
+        worker.work()
+
+        assert worker.item_names == ['P']
