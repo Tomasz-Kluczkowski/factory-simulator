07e594d53cb22dae0b50d4ab5ec4be4c28626546
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-12-02 23:22:53 +0000

    Normalize Worker - remove fields which duplicate information.
    factory_config and conveyor_belt are set on the factory_floor which
    already is an attribute of worker.

--- simulation/models/worker.py
+++ simulation/models/worker.py
@@ -2,8 +2,7 @@ from typing import List, Union
 
 from django.db import models
 
-from simulation.models import BaseModel, ConveyorBelt, Component, Product
-from simulation.models.factory_config import FactoryConfig
+from simulation.models import BaseModel, Component, Product
 
 
 class WorkerState:
@@ -11,6 +10,15 @@ class WorkerState:
     PICKING_UP = 'picking_up'
     DROPPING = 'dropping'
     BUILDING = 'building'
+
+#
+# class WorkerManager(models.Manager):
+#     def get_worker_group_for_factory_floor(self, factory_floor_id):
+#         return (
+#             self.get_queryset()
+#             .filter(factory_floor_id=factory_floor_id)
+#             .select_related('operation_times', )
+#         )
 
 
 class WorkerOperationTimes(BaseModel):
@@ -21,10 +29,8 @@ class WorkerOperationTimes(BaseModel):
 
 class Worker(BaseModel):
     name = models.CharField(max_length=30, blank=True, null=True, default='')
-    operation_times = models.ForeignKey(WorkerOperationTimes, on_delete=models.CASCADE, related_name='workers')
     slot_number = models.PositiveSmallIntegerField()
-    factory_config = models.ForeignKey(FactoryConfig, on_delete=models.CASCADE, related_name='workers')
-    conveyor_belt = models.ForeignKey(ConveyorBelt, on_delete=models.CASCADE, related_name='workers')
+    operation_times = models.ForeignKey(WorkerOperationTimes, on_delete=models.CASCADE, related_name='workers')
     factory_floor = models.ForeignKey('FactoryFloor', on_delete=models.CASCADE, related_name='workers')
 
     def __init__(self, *args, **kwargs):
@@ -91,6 +97,14 @@ class Worker(BaseModel):
     @property
     def item_names(self):
         return [item.name for item in self._items]
+
+    @property
+    def factory_config(self):
+        return self.factory_floor.factory_config
+
+    @property
+    def conveyor_belt(self):
+        return self.factory_floor.conveyor_belt
 
     def _can_pickup_component(self):
         return self.conveyor_belt.is_slot_free(self.slot_number)
