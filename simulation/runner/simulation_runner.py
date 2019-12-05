73ad881ee7d568c1a311b499f13c67d427c20420
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2019-12-05 23:01:14 +0000

    Fill in skeleton of prepare_simulation.

--- simulation/runner/simulation_runner.py
+++ simulation/runner/simulation_runner.py
@@ -3,7 +3,7 @@ Here I want to have an object that will use config provided by the front end and
 all the objects, save them in the DB and then have methods to run the optimisation and gather intermediate
 /end state etc.
 """
-from simulation.models import Feeder
+from simulation.models import Feeder, Receiver, FactoryConfig, ConveyorBelt, FactoryFloor
 
 
 class OptimisationRunner:
@@ -16,8 +16,16 @@ class OptimisationRunner:
         """
         # TODO: make this from either API or some other form of config.
         feeder = Feeder.objects.create()
-        receiver =
-
+        receiver = Receiver.objects.create()
+        factory_config = FactoryConfig.objects.create()
+        conveyor_belt = ConveyorBelt.objects.create(factory_config=factory_config)
+        factory_floor = FactoryFloor.objects.create(
+            factory_config=factory_config,
+            feeder=feeder,
+            receiver=receiver,
+            conveyor_belt=conveyor_belt
+        )
+        factory_floor.add_workers()
 
     def run_simulation(self):
         """
