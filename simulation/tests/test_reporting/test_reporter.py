5a85c3b77edee970418b50be3dbef4831dce6125
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-01-12 21:58:54 +0000

    Add tests for SimulationReporter class.

---
+++ simulation/tests/test_reporting/test_reporter.py
@@ -0,0 +1,49 @@
+import pytest
+from django.utils import timezone
+
+from simulation.tests.conftest import ItemFactory, FactoryConfigFactory, ReceiverFactory, SimulationReporterFactory
+
+pytestmark = pytest.mark.django_db
+
+
+class TestSimulationReporter:
+    @pytest.fixture
+    def factory_config(self):
+        return FactoryConfigFactory()
+
+    @pytest.fixture
+    def receiver(self):
+        return ReceiverFactory()
+
+    @pytest.fixture
+    def product_item(self, factory_config, receiver):
+        return ItemFactory(
+            name=factory_config.product_code, receiver=receiver, received_at=timezone.now()
+        )
+
+    def test_it_calculates_production_efficiency(self, factory_config, receiver, product_item):
+        non_product_items = [
+            ItemFactory(
+                name=factory_config.empty_code, receiver=receiver, received_at=timezone.now()
+            ) for __ in range(3)
+        ]
+
+        simulation_reporter = SimulationReporterFactory(receiver=receiver, factory_config=factory_config)
+
+        assert simulation_reporter.get_production_efficiency() == 1/4
+
+    def test_it_works_with_no_products(self, factory_config, receiver):
+        non_product_items = [
+            ItemFactory(
+                name=factory_config.empty_code, receiver=receiver, received_at=timezone.now()
+            ) for __ in range(3)
+        ]
+
+        simulation_reporter = SimulationReporterFactory(receiver=receiver, factory_config=factory_config)
+
+        assert simulation_reporter.get_production_efficiency() == 0
+
+    def test_it_works_with_no_items(self, factory_config, receiver):
+        simulation_reporter = SimulationReporterFactory(receiver=receiver, factory_config=factory_config)
+
+        assert simulation_reporter.get_production_efficiency() == 0
