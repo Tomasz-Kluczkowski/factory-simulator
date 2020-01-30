52469b34f2c1a5527fc1d62c9b56af36cd5396d7
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-01-30 22:40:26 +0000

    Add basic model serializer for factory config.

---
+++ api/serializers/factory_config.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from simulation.models import FactoryConfig
+
+
+class FactoryConfigSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = FactoryConfig
+        fields = [
+            'required_component_names',
+            'product_code',
+            'empty_code',
+            'number_of_simulation_steps',
+            'number_of_conveyor_belt_slots',
+            'number_of_worker_pairs',
+        ]
