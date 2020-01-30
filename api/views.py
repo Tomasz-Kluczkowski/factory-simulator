7c35128f858f224ca7f06607309c3dd4c3ae2d6a
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-01-30 22:54:38 +0000

    comment out api url / views as not ready and breaking the tests of the rest of the code.

--- api/views.py
+++ api/views.py
@@ -1,5 +1,5 @@
-from rest_framework import viewsets
-
-
-class FactoryConfigViewSet(viewsets.ModelViewSet):
-    pass
+# from rest_framework import viewsets
+#
+#
+# class FactoryConfigViewSet(viewsets.ModelViewSet):
+#     pass
--- factory_simulator/urls.py
+++ factory_simulator/urls.py
@@ -18,5 +18,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', )
+    # path('api/', include('api.urls', namespace='api')),
 ]
