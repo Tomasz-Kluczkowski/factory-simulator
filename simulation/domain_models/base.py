dec569b7b4e5bf891b4a95df17560708528099dc
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-01-30 22:58:39 +0000

    Create domain models package and base domain model abstract class.

---
+++ simulation/domain_models/base.py
@@ -0,0 +1,12 @@
+import abc
+
+
+class BaseModel(abc.ABC):
+    def __str__(self):
+        fields = ', '.join(
+            f'{field_name}={field_value}' for
+            field_name, field_value in self.__dict__.items() if not field_name.startswith('_')
+        )
+        representation = f'<{self.__class__.__name__}({fields})>'
+
+        return representation
