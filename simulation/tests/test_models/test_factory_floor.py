967189aa9ad2dd3eb8f3c45b7418b3c2c88eb839
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-01-30 23:36:37 +0000

    Update imports.

--- simulation/tests/test_models/test_factory_floor.py
+++ simulation/tests/test_models/test_factory_floor.py
@@ -4,7 +4,7 @@ import pytest
 
 from simulation.exceptions.exceptions import FactoryConfigError
 from simulation.models import FactoryFloor
-from simulation.models.feeder import sequential_feed_function
+from simulation.domain_models.feeder import sequential_feed_function
 from simulation.tests.conftest import FactoryFloorFactory, FeederFactory, ReceiverFactory, FactoryConfigFactory, \
     ConveyorBeltFactory, WorkerOperationTimesFactory, ItemFactory
 
