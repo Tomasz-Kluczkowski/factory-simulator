b15c0d63f7cd0500b852c5a10cd23ed0a74c5756
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-02-01 12:35:23 +0000

    Fix tests.

--- simulation/tests/test_reporting/test_reporter.py
+++ simulation/tests/test_reporting/test_reporter.py
@@ -16,15 +16,18 @@ class TestSimulationReporter:
         return ReceiverFactory()
 
     @pytest.fixture
-    def product_item(self, factory_config, receiver):
+    def product_item(self, factory_config):
         return ItemFactory(name=factory_config.product_code, received_at=timezone.now())
 
     def test_it_calculates_production_efficiency(self, factory_config, receiver, product_item):
-        [
+        items = [
             ItemFactory(
                 name=factory_config.empty_code, received_at=timezone.now()
             ) for __ in range(3)
         ]
+        items.append(product_item)
+        for item in items:
+            receiver.receive(item)
 
         simulation_reporter = SimulationReporterFactory(receiver=receiver, factory_config=factory_config)
 
