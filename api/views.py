6918407616519deffc29dfc6adbef6986776a538
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-02-01 23:39:15 +0000

    Create viewset for factory configs.

--- api/views.py
+++ api/views.py
@@ -1,5 +1,9 @@
-# from rest_framework import viewsets
-#
-#
-# class FactoryConfigViewSet(viewsets.ModelViewSet):
-#     pass
+from rest_framework import viewsets
+
+from api.serializers.factory_config import FactoryConfigSerializer
+from simulation.models.factory_config import FactoryConfig
+
+
+class FactoryConfigViewSet(viewsets.ModelViewSet):
+    serializer_class = FactoryConfigSerializer
+    queryset = FactoryConfig.objects.all()
