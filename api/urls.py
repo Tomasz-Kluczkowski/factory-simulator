35e7c3b86133defedc6e7982c21253426be3992f
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-02-01 23:38:53 +0000

    Add urls for api app.

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,13 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-#
-# from api.views import FactoryConfigViewSet
-#
-# router = DefaultRouter()
-# router.register(r'factory-configs', FactoryConfigViewSet)
-#
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
+from api.views import FactoryConfigViewSet
+
+app_name = 'api'
+
+router = DefaultRouter()
+router.register(r'factory-configs', FactoryConfigViewSet, basename='factory-configs')
+
+urlpatterns = [
+    path('', include(router.urls)),
+]
