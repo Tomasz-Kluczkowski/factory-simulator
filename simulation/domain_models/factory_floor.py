3708c0a4b89fc7baade4f749541a164cbb484c3d
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-02-01 20:46:47 +0000

    Convert FactoryFloor to a domain model.

--- simulation/domain_models/factory_floor.py
+++ simulation/domain_models/factory_floor.py
@@ -1,29 +1,26 @@
-from django.db import models
-
+from simulation.domain_models.base_domain_model import BaseDomainModel
 from simulation.domain_models.conveyor_belt import ConveyorBelt
 from simulation.domain_models.feeder import Feeder
 from simulation.domain_models.receiver import Receiver
 from simulation.exceptions.exceptions import FactoryConfigError
 from simulation.exceptions.messages import WRONG_FACTORY_CONFIG, INSUFFICIENT_FEED_INPUT
-from simulation.models import BaseModel, FactoryConfig
 from simulation.domain_models.worker import Worker
+from simulation.models.factory_config import FactoryConfig
 
 
-class FactoryFloor(BaseModel):
+class FactoryFloor(BaseDomainModel):
     """
     This is the controller of the entire operation. It will navigate the production line.
     By default the number of pairs matches the number of slots on the belt.
     """
-    factory_config = models.ForeignKey(FactoryConfig, on_delete=models.CASCADE, related_name='factory_floors')
-
     def __init__(
-            self, feeder: Feeder, receiver: Receiver, conveyor_belt: ConveyorBelt, *args, **kwargs,
+            self, feeder: Feeder, receiver: Receiver, conveyor_belt: ConveyorBelt, factory_config: FactoryConfig
     ):
         self._workers = []
         self.feeder = feeder
         self.receiver = receiver
         self.conveyor_belt = conveyor_belt
-        super().__init__(*args, **kwargs)
+        self.factory_config = factory_config
         if self.factory_config.number_of_worker_pairs > self.factory_config.number_of_conveyor_belt_slots:
             raise FactoryConfigError(WRONG_FACTORY_CONFIG)
         self.time = 0
@@ -32,11 +29,6 @@ class FactoryFloor(BaseModel):
         """
         Creates a pair of workers per self.factory_config.number_of_worker_pairs and saves in the database.
         Each worker pair is assigned to a slot_number on the conveyor belt.
-
-        Parameters
-        ----------
-        worker_operation_times
-            Operation times to be used per worker.
         """
         for slot_number in range(self.factory_config.number_of_worker_pairs):
             for pair_number in range(2):
