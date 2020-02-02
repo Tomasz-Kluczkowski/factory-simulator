3bb748a694f0ef4d721eee67fc38b110c5268ba5
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-02-02 12:26:49 +0000

    Add test settings.

--- factory_simulator/settings.py
+++ factory_simulator/settings.py
@@ -127,3 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# TESTING SETTINGS
+TESTING = config('TESTING', default=False, cast=bool)
+if TESTING:
+    # Allows to define temporary models during testing.
+    INSTALLED_APPS.append('test_application')
