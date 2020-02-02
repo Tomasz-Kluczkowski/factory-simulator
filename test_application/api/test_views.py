9df73e4ad166a9e494bb9c7445dd54fbb63c67d3
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-02-02 22:33:56 +0000

    Add tests for put, patch, delete endpoints for factory-configs. Use rest_framework's status to assert response's statuses.

--- test_application/api/test_views.py
+++ test_application/api/test_views.py
@@ -1,4 +1,4 @@
-from http import HTTPStatus
+from rest_framework import status
 
 import pytest
 from django.urls import reverse
@@ -14,8 +14,8 @@ class TestFactoryConfigViewSet:
         factory_config_1 = FactoryConfigFactory()
         factory_config_2 = FactoryConfigFactory()
 
-        response = client.get(reverse('api:factory-configs-list'))
-        assert response.status_code == HTTPStatus.OK
+        response = client.get(path=reverse('api:factory-configs-list'))
+        assert response.status_code == status.HTTP_200_OK
         assert len(response.data) == 2
 
         factory_configs = [factory_config_1, factory_config_2]
@@ -41,8 +41,8 @@ class TestFactoryConfigViewSet:
             'drop_time': 2,
             'build_time': 5,
         }
-        response = client.post(reverse('api:factory-configs-list'), factory_config_data)
-        assert response.status_code == HTTPStatus.CREATED
+        response = client.post(path=reverse('api:factory-configs-list'), data=factory_config_data)
+        assert response.status_code == status.HTTP_201_CREATED
 
         factory_config_id = response.data['id']
         factory_config = FactoryConfig.objects.get(id=factory_config_id)
@@ -60,7 +60,7 @@ class TestFactoryConfigViewSet:
         factory_config = FactoryConfigFactory()
 
         response = client.get(reverse('api:factory-configs-detail', kwargs={'pk': factory_config.pk}))
-        assert response.status_code == HTTPStatus.OK
+        assert response.status_code == status.HTTP_200_OK
 
         assert factory_config.id == response.data['id']
         assert factory_config.required_item_names == response.data['required_item_names']
@@ -71,3 +71,57 @@ class TestFactoryConfigViewSet:
         assert factory_config.pickup_time == response.data['pickup_time']
         assert factory_config.drop_time == response.data['drop_time']
         assert factory_config.build_time == response.data['build_time']
+
+    def test_put_detail_endpoint(self, client):
+        factory_config = FactoryConfigFactory()
+        factory_config_data = {
+            'id': factory_config.id,
+            'required_item_names': ['B', 'D'],
+            'product_code': 'Product',
+            'empty_code': 'Empty',
+            'number_of_simulation_steps': 20,
+            'number_of_worker_pairs': 4,
+            'pickup_time': 3,
+            'drop_time': 2,
+            'build_time': 5,
+        }
+
+        response = client.put(
+            path=reverse('api:factory-configs-detail', kwargs={'pk': factory_config.pk}),
+            data=factory_config_data,
+            content_type='application/json'
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+        updated_factory_config = FactoryConfig.objects.get(id=factory_config.id)
+        assert updated_factory_config.required_item_names == factory_config_data['required_item_names']
+        assert updated_factory_config.product_code == factory_config_data['product_code']
+        assert updated_factory_config.empty_code == factory_config_data['empty_code']
+        assert updated_factory_config.number_of_simulation_steps == factory_config_data['number_of_simulation_steps']
+        assert updated_factory_config.number_of_worker_pairs == factory_config_data['number_of_worker_pairs']
+        assert updated_factory_config.pickup_time == factory_config_data['pickup_time']
+        assert updated_factory_config.drop_time == factory_config_data['drop_time']
+        assert updated_factory_config.build_time == factory_config_data['build_time']
+
+    def test_patch_detail_endpoint(self, client):
+        factory_config = FactoryConfigFactory()
+        factory_config_data = {
+            'required_item_names': ['B', 'D'],
+        }
+
+        response = client.patch(
+            path=reverse('api:factory-configs-detail', kwargs={'pk': factory_config.pk}),
+            data=factory_config_data,
+            content_type='application/json'
+        )
+        assert response.status_code == status.HTTP_200_OK
+
+        updated_factory_config = FactoryConfig.objects.get(id=factory_config.id)
+        assert updated_factory_config.required_item_names == factory_config_data['required_item_names']
+
+    def test_delete_detail_endpoint(self, client):
+        factory_config = FactoryConfigFactory()
+        response = client.delete(path=reverse('api:factory-configs-detail', kwargs={'pk': factory_config.pk}))
+        assert response.status_code == status.HTTP_204_NO_CONTENT
+
+        assert not FactoryConfig.objects.filter(id=factory_config.id).exists()
