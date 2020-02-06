d63d69699ef015c36b8ce633749616d7ec9f5605
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-02-06 22:17:13 +0000

    Define Feeder's interface and concrete implementations.

--- simulation/domain_models/feeder.py
+++ simulation/domain_models/feeder.py
@@ -1,41 +1,64 @@
 import random
-from typing import Iterator, Callable, Tuple
+from abc import ABC, abstractmethod
+from typing import Iterator, Tuple
 
 from simulation.domain_models.base_domain_model import BaseDomainModel
 from simulation.domain_models.item import Item
 
 
-def sequential_feed_function(item_names: Tuple[str, ...]) -> Iterator['Item']:
-    return (Item(name=name) for name in item_names)
+class IFeeder(ABC):
+    @abstractmethod
+    def feed(self) -> Item:
+        """
+        Returns items one by one according to the pattern specified in the _feed_function.
+        """
+        pass
+
+    @abstractmethod
+    def _feed_function(self) -> Iterator[Item]:
+        """
+        Yields items in a specific pattern for the feed method.
+        """
+        pass
 
 
-class Feeder(BaseDomainModel):
-    """
-    Use to provide feed for the conveyor belt.
-
-    If feed_function is specified then it will be used to generate items.
-    If no feed_function is provided we randomly generate an item with name from item_names and yield it.
-    """
-    def __init__(
-            self,
-            item_names: Tuple[str, ...] = ('A', 'B'),
-            feed_function: Callable[[Tuple[str, ...]], Iterator[Item]] = None,
-    ):
+class Feeder(IFeeder, BaseDomainModel):
+    def __init__(self, item_names: Tuple[str, ...] = ('A', 'B')):
         self._item_names = item_names
-        self._feed_function = (
-            feed_function(item_names=item_names) if feed_function else self._default_feed_function()
-        )
+        self._pattern = self._feed_function()
 
-    def _default_feed_function(self) -> Iterator[Item]:
+    def feed(self) -> Item:
+        """
+        Returns items one by one according to the pattern specified in the _pattern.
+        """
+        item = next(self._pattern)
+        return item
+
+    def _feed_function(self) -> Iterator[Item]:
+        """
+        Yields items in a specific pattern for the feed method.
+        """
+        raise NotImplementedError
+
+
+class RandomizedFeeder(Feeder):
+    """
+    Use to provide feed for the conveyor belt in random order by creating an item with a name from item_names.
+    """
+    def _feed_function(self) -> Iterator[Item]:
+        """
+        Yields items one by one in random order.
+        """
         while True:
             item = Item(
                 name=random.choice(self._item_names),
             )
             yield item
 
-    def feed(self) -> Item:
-        """
-        Returns items according to __feed_function and sets the feeder attribute on each item if missing.
-        """
-        item = next(self._feed_function)
-        return item
+
+class SequentialFeeder(Feeder):
+    """
+    Use to provide a sequential feed based on order of _item_names for the conveyor belt.
+    """
+    def _feed_function(self) -> Iterator[Item]:
+        return (Item(name=name) for name in self._item_names)
