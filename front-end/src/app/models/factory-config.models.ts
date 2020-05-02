950e9a31567d19b8822c4153235da412809ec6e6
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-05-02 20:11:04 +0100

    Create interface for factory config response and resource.

---
+++ front-end/src/app/models/factory-config.models.ts
@@ -0,0 +1,25 @@
+import {ApiResource, ApiResponse} from './api.models';
+
+export interface FactoryConfigAPIResponse extends ApiResponse {
+  required_item_names: string[];
+  product_code: string;
+  empty_code: string;
+  number_of_simulation_steps: number;
+  number_of_conveyor_belt_slots: number;
+  number_of_worker_pairs: number;
+  pickup_time: number;
+  drop_time: number;
+  build_time: number;
+}
+
+export interface FactoryConfigAPIResource extends ApiResource {
+  requiredItemNames: string[];
+  productCode: string;
+  emptyCode: string;
+  numberOfSimulationSteps: number;
+  numberOfConveyorBeltSlots: number;
+  numberOfWorkerPairs: number;
+  pickupTime: number;
+  dropTime: number;
+  buildTime: number;
+}
