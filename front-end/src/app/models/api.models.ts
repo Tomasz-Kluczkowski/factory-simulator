98a5b8f48b9346bf928494c28186cea864526468
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-05-02 23:33:46 +0100

    Add RequestOptions interface.

--- front-end/src/app/models/api.models.ts
+++ front-end/src/app/models/api.models.ts
@@ -1,3 +1,5 @@
+import {HttpHeaders, HttpParams} from '@angular/common/http';
+
 export interface ApiResponse {
   id: number;
 }
@@ -5,4 +7,14 @@ export interface ApiResponse {
 
 export interface ApiResource {
   id: number;
+}
+
+
+export interface RequestOptions {
+    headers?: HttpHeaders | {[header: string]: string | string[]};
+    observe?: any;
+    params?: HttpParams|{[param: string]: string | string[]};
+    reportProgress?: boolean;
+    responseType?: any;
+    withCredentials?: boolean;
 }
