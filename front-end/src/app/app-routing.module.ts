f4a20bb6b26616fddb9f9b88cf6ca591537d2cad
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-05-02 20:12:35 +0100

    Update imports.

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {HomeComponent} from './components/home/home.component';
+import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
