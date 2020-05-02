acfcb98a5b439c67fa446fb797e8389a1d9a9c40
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-05-02 20:12:03 +0100

    Create FactoryConfigSerializer.

---
+++ front-end/src/app/serializers/factory-config.serializer.ts
@@ -0,0 +1,34 @@
+import {ApiSerializer} from './api.serializer';
+import {FactoryConfigAPIResource, FactoryConfigAPIResponse} from '../models/factory-config.models';
+
+export class FactoryConfigSerializer implements ApiSerializer {
+  fromApiResponse(apiResponse: FactoryConfigAPIResponse): FactoryConfigAPIResource {
+    return {
+      id: apiResponse.id,
+      requiredItemNames: apiResponse.required_item_names,
+      productCode: apiResponse.product_code,
+      emptyCode: apiResponse.empty_code,
+      numberOfSimulationSteps: apiResponse.number_of_simulation_steps,
+      numberOfConveyorBeltSlots: apiResponse.number_of_conveyor_belt_slots,
+      numberOfWorkerPairs: apiResponse.number_of_worker_pairs,
+      pickupTime: apiResponse.pickup_time,
+      dropTime: apiResponse.drop_time,
+      buildTime: apiResponse.build_time
+    };
+  }
+
+  toApiResponse(apiResource: FactoryConfigAPIResource): FactoryConfigAPIResponse {
+    return {
+      id: apiResource.id,
+      required_item_names: apiResource.requiredItemNames,
+      product_code: apiResource.productCode,
+      empty_code: apiResource.emptyCode,
+      number_of_simulation_steps: apiResource.numberOfSimulationSteps,
+      number_of_conveyor_belt_slots: apiResource.numberOfConveyorBeltSlots,
+      number_of_worker_pairs: apiResource.numberOfWorkerPairs,
+      pickup_time: apiResource.pickupTime,
+      drop_time: apiResource.dropTime,
+      build_time: apiResource.buildTime
+    };
+  }
+}
