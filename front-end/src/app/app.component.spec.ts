695e872585a0db57f8d3a1ef7c683fbfcc839de1
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-05-06 23:47:39 +0100

    Change title to experiment with testing.

--- front-end/src/app/app.component.spec.ts
+++ front-end/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'front-end'`, () => {
+  it(`should have as title 'Factory Floor Simulator'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('front-end');
+    expect(app.title).toEqual('Factory Floor Simulator');
   });
 
   it('should render title', () => {
