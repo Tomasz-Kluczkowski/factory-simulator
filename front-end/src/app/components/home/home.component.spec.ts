da5b20e45d0244ec071765a425cc69fa1d62842f
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-05-06 23:48:30 +0100

    Fix test for home component.

--- front-end/src/app/components/home/home.component.spec.ts
+++ front-end/src/app/components/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import {FactoryConfigService} from '../../services/factory-config/factory-config.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,7 +9,12 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [ HomeComponent ],
+      providers: [
+        {
+          provide: FactoryConfigService,
+        }
+      ],
     })
     .compileComponents();
   }));
