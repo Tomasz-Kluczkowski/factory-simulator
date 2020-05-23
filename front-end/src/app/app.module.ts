5b80c232dd45ebefa930f17927c1b31a631ecec8
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-05-23 16:59:28 +0100

    Adjust imports of moved modules.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
+import {AppRoutingModule} from './modules/app-routing.module';
 import {AppComponent} from './app.component';
 import {HttpClientModule} from '@angular/common/http';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
@@ -9,14 +9,18 @@ import {HomeComponent} from './components/home/home.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FlexLayoutModule} from '@angular/flex-layout';
 import { NavigationComponent } from './components/navigation/navigation.component';
-import {MaterialModule} from './material.module';
+import {MaterialModule} from './modules/material.module';
+import {AboutComponent} from './components/about/about.component';
+import { ContactComponent } from './components/contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PageNotFoundComponent,
     HomeComponent,
-    NavigationComponent
+    NavigationComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
