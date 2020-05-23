27be03df4de204e2d13d153a93fe0aad497d62e3
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-05-23 16:43:20 +0100

    Use PathResolveService for page not found route. Redirect to /home if path is root.

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './components/home/home.component';
 import {PageNotFoundComponent} from './components/page-not-found/page-not-found.component';
+import {PathResolveService} from './services/path-resolve/path-resolve.service';
+import {paths} from './configuration/app-paths';
 
 
 const routes: Routes = [
   {
     path: '',
+    redirectTo: paths.home,
+    pathMatch: 'full'
+  },
+  {
+    path: paths.home,
     component: HomeComponent,
   },
   {
     path: '**',
+    resolve: {
+      path: PathResolveService
+    },
     component: PageNotFoundComponent,
   },
 ];
