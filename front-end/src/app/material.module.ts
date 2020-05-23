be3062d55d7e53a6e6f67e3efebac688ea518dda
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2020-05-23 13:46:38 +0100

    Add CommonModule.

--- front-end/src/app/material.module.ts
+++ front-end/src/app/material.module.ts
@@ -1,9 +1,11 @@
 import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   imports: [
+    CommonModule,
     MatButtonModule,
     MatToolbarModule
   ],
