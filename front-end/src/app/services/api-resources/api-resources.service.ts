e7158f68640a3d012accafc622044b19f2f2a938
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-01-23 23:02:28 +0000

    Create generic create method

--- front-end/src/app/services/api-resources/api-resources.service.ts
+++ front-end/src/app/services/api-resources/api-resources.service.ts
@@ -2,14 +2,14 @@ import {environment} from '../../../environments/environment';
 import {HttpClient} from '@angular/common/http';
 import {ApiSerializer} from '../../serializers/api.serializer';
 import {Observable} from 'rxjs';
-import {RequestOptions} from '../../models/api.models';
+import {ApiResource, RequestOptions} from '../../models/api.models';
 import {map} from 'rxjs/operators';
 
 
 const API_URL = environment.apiURL;
 
 
-export abstract class ApiResourcesService<T> {
+export abstract class ApiResourcesService<T extends ApiResource> {
 
   protected constructor(
     protected httpClient: HttpClient,
@@ -23,9 +23,20 @@ export abstract class ApiResourcesService<T> {
       .get(this.getUrl(this.endpoint), requestOptions)
       .pipe(
         map(
-          (apiData: any) => apiData.map(apiResponse => this.serializer.fromApiResponse(apiResponse))
+          (apiResponse: any) => apiResponse.map(apiResponseItem => this.serializer.fromApiResponse(apiResponseItem))
         )
       );
+  }
+
+  create(data: any, requestOptions?: RequestOptions): Observable<T> {
+    return this.httpClient
+      .post(this.getUrl(this.endpoint), this.serializer.toApiResponse(data), requestOptions)
+      .pipe(
+        map(
+          (apiResponse: any) => {
+            return <T>this.serializer.fromApiResponse(apiResponse);
+          })
+        )
   }
 
   getUrl(endpoint: string, id: any = ''): string {
