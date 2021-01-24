9a4c9181a1d44769e29ad6bd32dfe245cda0a333
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-01-24 13:55:11 +0000

    Rework models.

--- simulation/admin.py
+++ simulation/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
 from simulation.models.factory_config import FactoryConfig
+from simulation.models.result import Result
+from simulation.models.simulation import Simulation
 
 admin.site.register(FactoryConfig)
+admin.site.register(Result)
+admin.site.register(Simulation)
---
+++ simulation/models/result.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+from simulation.models.base_model import BaseModel
+from simulation.models.simulation import Simulation
+
+
+class Result(BaseModel):
+    efficiency = models.DecimalField(max_digits=4, decimal_places=2)
+    simulation = models.ForeignKey(Simulation, on_delete=models.CASCADE)
