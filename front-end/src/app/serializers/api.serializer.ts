0331c364b31bcc8ae066ff06135c64a22670c750
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-01-24 19:05:00 +0000

    Remove serializers - the backend sends data in format we need.

--- front-end/src/app/serializers/api.serializer.ts
+++
@@ -1,7 +0,0 @@
-import {ApiResource, ApiResponse} from '../models/api.models';
-
-
-export interface ApiSerializer {
-  fromApiResponse(apiResponse: ApiResponse): ApiResource;
-  toApiResponse(apiResource: ApiResource): ApiResponse;
-}
