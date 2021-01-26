5c7ad34b25edda87350b2bce044cb5f6c0577a83
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-01-26 22:50:00 +0000

    Update settings and urls to make django debug toolbar work.

--- factory_simulator/urls.py
+++ factory_simulator/urls.py
@@ -16,7 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from factory_simulator import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls', namespace='api')),
 ]
+
+if settings.DEBUG:  # pragma: no cover
+    import debug_toolbar
+    urlpatterns = [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
