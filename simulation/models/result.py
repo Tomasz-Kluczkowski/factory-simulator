555ab114ddd33e6db873b25d7b3c9fdd67eabebe
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-01-26 22:50:19 +0000

    Specify related name.

--- simulation/models/result.py
+++ simulation/models/result.py
@@ -6,4 +6,4 @@ from simulation.models.simulation import Simulation
 
 class Result(BaseModel):
     efficiency = models.DecimalField(max_digits=4, decimal_places=2)
-    simulation = models.ForeignKey(Simulation, on_delete=models.CASCADE)
+    simulation = models.ForeignKey(Simulation, on_delete=models.CASCADE, related_name='results')
