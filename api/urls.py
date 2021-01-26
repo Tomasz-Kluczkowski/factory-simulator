de7f8abeb78ac449625990c14ed347fe00ee3799
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-01-26 22:50:42 +0000

    Update imports after moving viewsets to separate files.

--- api/urls.py
+++ api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from api.views import FactoryConfigViewSet
+from api.views.factory_config import FactoryConfigViewSet
+from api.views.simulation import SimulationViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register(r'factory-configs', FactoryConfigViewSet, basename='factory-configs')
+router.register(r'simulations', SimulationViewSet, basename='simulations')
 
 urlpatterns = [
     path('', include(router.urls)),
