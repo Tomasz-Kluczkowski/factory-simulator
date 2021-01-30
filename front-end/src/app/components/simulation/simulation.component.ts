17359b1a7ee9411942880c918bd354e100774454
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-01-30 21:24:06 +0000

    Change provider to simulation api service. Remove factory config from constructor - it is not needed here.

--- front-end/src/app/components/simulation/simulation.component.ts
+++ front-end/src/app/components/simulation/simulation.component.ts
@@ -1,17 +1,17 @@
 import {Component, OnInit} from '@angular/core';
 import {FactoryConfigAPIResource} from '../../models/factory-config.models';
-import {FactoryConfigService} from '../../services/factory-config/factory-config.service';
+import {SimulationAPIService} from '../../services/api/simulation/simulation-api.service';
 
 @Component({
   selector: 'app-simulation',
-  providers: [FactoryConfigService],
+  providers: [SimulationAPIService],
   templateUrl: './simulation.component.html',
   styleUrls: ['./simulation.component.scss']
 })
 export class SimulationComponent implements OnInit {
   factoryConfigs: FactoryConfigAPIResource[];
 
-  constructor(private factoryConfigService: FactoryConfigService) {
+  constructor() {
   }
 
   ngOnInit(): void {}
