4e374d849307ca2ed2b367825954cad58a694a3a
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-01-30 21:24:52 +0000

    Create simulation and result api resource models.

---
+++ front-end/src/app/models/simulation.models.ts
@@ -0,0 +1,10 @@
+import {ApiResource} from './api.models';
+import {FactoryConfigAPIResource} from './factory-config.models';
+import {ResultAPIResource} from './result.models';
+
+export interface SimulationAPIResource extends ApiResource {
+  name: string;
+  description: string;
+  factoryConfigs: FactoryConfigAPIResource[];
+  results: ResultAPIResource[];
+}
