7a829b300080085570ca373782ae4787a1474b42
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-02-09 23:39:35 +0000

    Create basic notification service.

---
+++ front-end/src/app/services/notification/notification.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(public snackbar: MatSnackBar) { }
+
+  showSuccess(message: string): void {
+    this.snackbar.open(message, '', {duration: 3000, panelClass: ['notification-service-success']});
+  }
+
+  showFailure(message: string): void {
+    this.snackbar.open(message, '', {duration: 5000, panelClass: ['notification-service-error']})
+  }
+}
