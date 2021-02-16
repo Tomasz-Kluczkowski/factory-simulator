5ca95a990730bc2f0d6055810dcc3b3165ffb29f
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-02-16 23:13:16 +0000

    Add more detail.

--- front-end/src/app/components/simulation/simulation-detail-view/simulation-detail-view.component.ts
+++ front-end/src/app/components/simulation/simulation-detail-view/simulation-detail-view.component.ts
@@ -1,5 +1,9 @@
 import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, ParamMap} from '@angular/router';
+import {SimulationAPIService} from '../../../services/api/simulation/simulation-api.service';
+import {Observable} from 'rxjs';
+import {SimulationAPIResource} from '../../../models/simulation.models';
+import {switchMap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-simulation-detail-view',
@@ -8,11 +12,19 @@ import {ActivatedRoute} from '@angular/router';
 })
 export class SimulationDetailViewComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private simulationAPIService: SimulationAPIService
+  ) { }
+
+  simulation$: Observable<SimulationAPIResource>
 
   @Input('simulation')
 
   ngOnInit(): void {
+    this.simulation$ = this.activatedRoute.paramMap.pipe(
+      switchMap((params: ParamMap) => this.simulationAPIService.get(parseInt(params.get('id'))))
+    );
   }
 
 }
