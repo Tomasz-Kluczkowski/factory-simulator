075ac642443c21a6cdbfe37aed741903a70bc10d
Tomasz-Kluczkowski <tomaszk1@hotmail.co.uk>
2021-03-06 22:30:48 +0000

    Add temporary code to trigger simulations from the UI.

--- api/views/simulation.py
+++ api/views/simulation.py
@@ -2,8 +2,21 @@ from rest_framework import viewsets
 
 from api.serializers.simulation import SimulationSerializer
 from simulation.models.simulation import Simulation
+from simulation.tasks.tasks import run_simulation
 
 
 class SimulationViewSet(viewsets.ModelViewSet):
     serializer_class = SimulationSerializer
     queryset = Simulation.objects.prefetch_related('factory_configs', 'results')
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+
+        # TODO: create a dedicated endpoint for starting simulation and use it in the detail view in UI once the objects
+        #  are successfully persisted - give a button. Initially just to run 1 factory config. Once we can do that -
+        #  enable running each config in a simulation separately and have a run-all button as well.
+
+        # TODO: move starting of the simulation into a dedicated endpoint away from here!
+        run_simulation.delay(response.data['id'])
+
+        return response
